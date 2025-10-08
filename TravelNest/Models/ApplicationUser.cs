d49278b6b1e2014ccb3a955ee2d93a73307925e0
Mihai Iordache <mihaiird14@gmail.com>
2025-10-08 14:18:52 +0300

    Add project files.

---
+++ TravelNest/Models/ApplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace TravelNest.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+
+        [Required]
+        [StringLength(50)]
+        [MinLength(5)]
+        public string FirstName { set; get; }
+
+        [Required]
+        [StringLength(50)]
+        [MinLength(5)]
+        public string LastName { set; get; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date Of Birth")]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ TravelNest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TravelNest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
